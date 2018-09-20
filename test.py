c5878d6281a128a5ea3ebf160c527997d7b33817
Antonio Martínez García <39262428+antoniomg89@users.noreply.github.com>
2018-09-20 16:23:18 +0200

    Arreglado el código de ejemplo

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import qrcode
 
 
 
-codigo = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 3,
+codigo_qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 3,
 )
 
 info = "Información dentro del código QR"
 
-qr.add_data(info)
-qr.make(fit=True)
+codigo_qr.add_data(info)
+codigo_qr.make(fit=True)
 
-imagen_qr = qr.make_image()
-imgagen_qr.save("image.png")
+imagen_qr = codigo_qr.make_image()
+imgagen_qr.save("imagen_qr.png")
