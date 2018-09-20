be5f0f29ca5c2c758b296e977e45f79bf28af223
Antonio Martínez García <39262428+antoniomg89@users.noreply.github.com>
2018-09-20 16:19:18 +0200

    Añadida función qr estática básica

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
 # Ejemplo de codigo pyhton
-import sys
-print (sys.platform)
+import qrcode
+
+
+
+codigo = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 10, border = 3,
+)
+
+info = "Información dentro del código QR"
+
+qr.add_data(info)
+qr.make(fit=True)
+
+imagen_qr = qr.make_image()
+imgagen_qr.save("image.png")
