b48ec06e7f1208ad7347ee844163acef78412dcf
Antonio Martínez García <amgarcia@correo.ugr.es>
2018-10-05 11:09:32 +0200

    Archivos de prueba borrados

--- test_generaQR.py
+++
@@ -1,9 +0,0 @@
-# Ejemplo de codigo pyhton
-import generaQR
-
-def test_genQR():
-    assert generaQR.genQR('información') == 'información'
-
-def test_valQR():
-    info = generaQR.valQR('validarinformacion')
-    assert type(info) is str
